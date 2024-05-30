625e90d9f3f8024e93587e7c5936e1e57ce6479b
Omar Tabengwa <166306416+omartabz@users.noreply.github.com>
2024-05-30 15:19:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import handlebars from 'vite-plugin-handlebars';
 import data from './data.json';
 
 export default {
-  plugins: [
-    handlebars({
-      context: {}, // replace the curly brackets with data.json
-    }),
-  ],
+  plugins: [handlebars({
+    context : data
+  })]
 };
+
+// export default {
+//     base : '/responsive_cv/',   // <-- add this
+//     plugins: [handlebars({
+//         context : data   // add this
+//     })],
+// };
